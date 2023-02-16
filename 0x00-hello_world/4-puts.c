a770fd56e8ec146e46f1f137f506447fbd0475c8
Xantrophy <mieraf238@gmail.com>
2023-02-16 07:56:07 -0800

    file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+*main - entry point
+*
+*Description: 'printing'
+*
+*Return: - void (sucess)
+*/
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle");
