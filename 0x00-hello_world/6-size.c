a67d03c2746021bd96a17789b9a231345af1d972
Xantrophy <mieraf238@gmail.com>
2023-02-16 08:55:18 -0800

    file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int intType;
 long int longIntType;
 long long int longLongIntType;
 float floatType;
-printf("Size of char: %zu bytes\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of long int: %zu bytes\n", sizeof(longIntType));
-printf("Size of long long int: %zu byte\n", sizeof(longLongIntType));
-printf("Size of float: %zu byte\n", sizeof(floatType));
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of int: %zu byte(s)\n", sizeof(intType));
+printf("Size of long int: %zu byte(s)\n", sizeof(longIntType));
+printf("Size of long long int: %zu byte(s)\n", sizeof(longLongIntType));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
