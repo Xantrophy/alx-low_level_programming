0161603f652651155e4d42c05a9572aba00a1036
Xantrophy <mieraf238@gmail.com>
2023-02-17 08:58:24 -0800

    file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,8 +11,9 @@
 int main(void)
 {
 	char letter;
-for (letter = 'a'; letter < 'A'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
-
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
 	return (0);
 }
