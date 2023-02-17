f17d44a2899774f082edc59a9cde871e20f6037a
Xantrophy <mieraf238@gmail.com>
2023-02-17 10:03:47 -0800

    file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x;
+char letter;
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+}
+for (letter = 'a'; letter <= 'f'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
