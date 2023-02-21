74e458065818d37b507aed84c816cc67f74c305d
Xantrophy <mieraf238@gmail.com>
2023-02-21 13:23:06 -0800

    file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 /**
  * print_to_98 - Check Holberton
- *
+ * @n: Number accepted from user
  *
  * Description: Function that adds two integers and returns the result
  * Return: Result to add a and b
  */
-void print_to_98(void)
+void print_to_98(int n)
 {
-for (i =0 ; i<=98 ; i++)
+for (i = n ; i<=98 ; i++)
 {
 printf("%d, ", i);
 }
