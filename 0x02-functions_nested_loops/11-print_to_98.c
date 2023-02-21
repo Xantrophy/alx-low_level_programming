3088688642fd48e5456649a9d0114ea1fec4d0f3
Xantrophy <mieraf238@gmail.com>
2023-02-21 13:17:32 -0800

    file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Check Holberton
+ *
+ *
+ * Description: Function that adds two integers and returns the result
+ * Return: Result to add a and b
+ */
+void print_to_98(int a, int b)
+{
+  for (i =0 ; i<=98 ; i++)
+    {
+      printf("%d, ", i)
+    }
+}
