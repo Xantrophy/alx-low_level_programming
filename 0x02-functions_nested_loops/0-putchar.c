e36b5f4c898a3cbac17b9f6197601ad7ed9b3f1a
Xantrophy <mieraf238@gmail.com>
2023-02-21 08:23:19 -0800

    file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char ch[8] = "_putchar";
+int i;
+for (i = 0 ; i < 8 ; i++)
+{
+putchar(ch[i]);
+}
+putchar('\n');
+return (0);
+}
