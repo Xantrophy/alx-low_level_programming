fa702a206141d5188884655d06488453e3643480
Xantrophy <mieraf238@gmail.com>
2023-02-21 12:10:57 -0800

    file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - Check description
+ * @n: An input character
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
+ */
+int print_last_digit(int n)
+{
+int x;
+x = n % 10;
+_putchar('x')
+}
