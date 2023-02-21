a0acffcc46f0d27eb1a1fa0a83e8cf73fa2cb36d
Xantrophy <mieraf238@gmail.com>
2023-02-21 11:11:30 -0800

    file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - Check description
+ * @n: An input character
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
+ */
+int print_sign(int n)
+{
+char i;
+if (n > 0)
+{
+printf("%c", '+');
+return (1);
+}
+else if (n == 0)
+{
+printf("%d", 0);
+return (0);
+}
+else if (n < 0)
+{
+printf("%c", '-');
+return (-1);
+}
+}
