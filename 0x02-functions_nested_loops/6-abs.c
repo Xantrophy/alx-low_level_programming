5d8f8e38d1a50f2e9840870279987a399595775c
Xantrophy <mieraf238@gmail.com>
2023-02-21 11:44:23 -0800

    file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,22 +9,18 @@
 int _abs(int n)
 {
 int y;
-int x;
 if (n > 0)
 {
 printf("%d", n);
-x = 1;
 }
 else if (n == 0)
 {
 printf("%d", n);
-x = 0;
 }
 else if (n < 0)
 {
 y = n * -1;
 printf("%d", y);
-x = -1;
 }
-return (x);
+return (0);
 }
