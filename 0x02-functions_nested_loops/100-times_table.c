27e67817a29423159acac7d019cf2ef6d3ae3b7f
Xantrophy <mieraf238@gmail.com>
2023-02-22 10:17:53 -0800

    file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,11 +14,15 @@ void print_times_table(int n)
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			for (j = 0; j <= n; j++)
+			for (j = 1; j <= n; j++)
 				putformat(i * j);
 			_putchar('\n');
 		}
 	}
+if (n = 0)
+{
+printf("%d", 0);
+}
 }
 
 /**
