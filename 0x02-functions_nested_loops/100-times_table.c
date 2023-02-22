c75025ee1b6bd2abdd54d816ba099c78c06ea875
Xantrophy <mieraf238@gmail.com>
2023-02-22 09:41:50 -0800

    file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,13 +13,19 @@ int j;
 for (i = 1 ; i <= n ; i++)
 {
 printf("%d,   ", 0);
-printf("%c", '\n');
 }
 for (i = 1 ; i <= n ; i++)
 {
 for (j = 0 ; j <= i * n ; j += i)
 {
-printf("%d,   ", j);
+if (j != n)
+}
+printf("%d,  ", j);
+}
+else
+{
+printf("%d", j);
+}
 if (n < 0 || n > 15)
 {
 break;
