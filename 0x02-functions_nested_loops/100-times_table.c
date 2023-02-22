aad13480223e4dcd1a39d886e8b5bab677fbeed1
Xantrophy <mieraf238@gmail.com>
2023-02-22 10:02:59 -0800

    file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,22 +10,37 @@ void print_times_table(int n)
 {
 int i;
 int j;
-for (i = 1 ; i <= n + 1 ; i++)
+/**
+ * putformat - formatted characters to output
+ * @n: number to format
+ * Return: nothing
+ */
+void putformat(int n)
 {
-printf("%d,   ", 0);
-}
-printf("%c", '\n');
-for (i = 1 ; i <= n ; i++)
-{
-for (j = 0 ; j <= i * n ; j += i)
-{
-putformat(j);
-if (n < 0 || n > 15)
-{
-break;
-}
-}
-printf("%c", '\n');
+	if (n <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
 }
 }
 /**
