b8552923d0d96b4961d74135133b1f4710a8b557
Xantrophy <mieraf238@gmail.com>
2023-02-22 08:53:15 -0800

    file

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - Check Holberton
+ * @n: An integer input
+ * Description: This function prints the last digit of a number
+ * Return: last digit of number r
+ */
+void print_times_table(int n)
+{
+int i;
+int j;
+for (i = 0 ; i < n ; i++)
+{
+for (j = 0 ; j < i * n ; j += i)
+printf("%d, ", j);
+if (n < 0 || n > 15)
+{
+break;
+}
+}
+}
