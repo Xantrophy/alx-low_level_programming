73d4b4558f08c1ae23a2e644877bcec7a9882740
Xantrophy <mieraf238@gmail.com>
2023-02-22 09:50:39 -0800

    file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,18 +18,11 @@ for (i = 1 ; i <= n ; i++)
 {
 for (j = 0 ; j <= i * n ; j += i)
 {
-if (j != n)
-{
-printf("%d,  ", j);
-}
-else
-{
-printf("%d", j);
-}
 if (n < 0 || n > 15)
 {
 break;
 }
 }
+printf("%c", '\n');
 }
 }
