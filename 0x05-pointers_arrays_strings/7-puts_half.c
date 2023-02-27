c788f37db2771ee5d5f5409a68131675dc970ab2
Xantrophy <mieraf238@gmail.com>
2023-02-27 10:25:53 -0800

    file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts_half - a function that prints half of a string, followed by a new line.
+ * @str: An input string
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int len = 0, i, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	while (i > (len / 2) && i <= len)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
