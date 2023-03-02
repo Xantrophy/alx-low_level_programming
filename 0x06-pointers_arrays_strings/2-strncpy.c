d2ee247a9105385a2d8cfdf262bb981cacdf45f4
Xantrophy <mieraf238@gmail.com>
2023-03-02 09:05:47 -0800

    file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _strncpy - a function that copys the strings.
@@ -9,10 +9,24 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
-while ((*src = *dest) != '\0') {
-src++;
-dest++;
-}
-  
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	srclen++;
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	return (temp);
 }
