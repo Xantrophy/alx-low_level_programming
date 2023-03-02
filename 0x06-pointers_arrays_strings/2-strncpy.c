d83b8d45b92b1fddbc3e063557a92f18f76fbb3c
Xantrophy <mieraf238@gmail.com>
2023-03-02 08:38:05 -0800

    file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,41 +1,18 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _strncpy - a function that copys the strings.
+ * @dest: an input string
+ * @src: an input string
+ * @n: an input integer
+ * Return: A pointer to the resulting string
  */
-int main(void)
+char *_strncpy(char *dest, char *src, int n)
 {
-  char s1[98];
-  char *p;
-  int i;
-
-  for (i = 0; i < 98 - 1; i++)
-    {
-      s1[i] = '*';
-    }
-  s1[i] = '\0';
-  printf("%s\n", s1);
-  p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-  printf("%s\n", s1);
-  printf("%s\n", p);
-  p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-  printf("%s", s1);
-  printf("%s", p);
-  for (i = 0; i < 98; i++)
-    {
-      if (i % 10)
-        {
-	  printf(" ");
-        }
-      if (!(i % 10) && i)
-        {
-	  printf("\n");
-        }
-      printf("0x%02x", s1[i]);
-    }
-  printf("\n");
-  return (0);
+int i = 0;
+while ((*src = *dest) != '\0') {
+s++;
+t++;
+}
+  
 }
